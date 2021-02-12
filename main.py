1a8d41bcdec77d30437884f5b34cafc29e3d2a0d
Borja Martínez Tadeo <bmartinez059@ikasle.ehu.eus>
2021-02-12 19:35:24 +0100

    Update main.py
    BMT: Se añade la version v1.1 que permite buscar los positivos por municipio.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from emoji import emojize
 
-token = '1606215435:AAGciyTG0jI0jKyyx7_xNErjCh2jaZzaPzE'
+token = 'TOKEN'
 bot = telebot.TeleBot(token)
 
 urlIncidenciaEuskadi = 'https://www.geo.euskadi.eus/geoeuskadi/rest/services/COVID19/COVID19_v2/MapServer/2/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=1000'
